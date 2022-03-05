dd121e111aa96bcc9f1c565e69c590be7df568ea
周成柱 <1073355312@qq.com>
2022-03-06 01:35:45 +0800

    feat: hw, ha, hb, hi, hc 5个快捷键

--- lua/conf/hop.lua
+++ lua/conf/hop.lua
@@ -2,8 +2,15 @@
 require("hop").setup()
 
 -- 搜索并跳转到单词
-vim.keybinds.gmap("n", "<leader>hw", "<cmd>HopWord<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "hw", "<cmd>HopWord<CR>", vim.keybinds.opts)
+
+-- 向前搜索, after
+vim.keybinds.gmap("n", "ha", "<cmd>HopWordAC<CR>", vim.keybinds.opts)
+
+-- 向后搜搜， word
+vim.keybinds.gmap("n", "hb", "<cmd>HopWordBC<CR>", vim.keybinds.opts)
+
 -- 搜索并跳转到行
-vim.keybinds.gmap("n", "<leader>hl", "<cmd>HopLine<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "hi", "<cmd>HopLine<CR>", vim.keybinds.opts)
 -- 搜索并跳转到字符
-vim.keybinds.gmap("n", "<leader>hc", "<cmd>HopChar1<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "hc", "<cmd>HopChar1<CR>", vim.keybinds.opts)
