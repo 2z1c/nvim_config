b40b7299d66261972b5749871c0eaf5d1c14ef90
周成柱 <1073355312@qq.com>
2022-03-06 02:09:47 +0800

    nothing

--- lua/conf/telescope.lua
+++ lua/conf/telescope.lua
@@ -1,4 +1,4 @@
--- https://github.com/nvim-telescope/telescope.nvim​
+-- https://github.com/nvim-telescope/telescope.nvim
 -- WARN: telescope 手动安装依赖 fd 和 repgrep
 -- https://github.com/sharkdp/fd
 -- https://github.com/BurntSushi/ripgrep
